7a7068b836ecaac7890eeb576e3e017ab68ad481
grestre <98605763+grestre@users.noreply.github.com>
2022-02-21 13:28:35 -0500

    Add files via upload

---
+++ MovieDriver.java
@@ -0,0 +1,33 @@
+import java.util.Scanner; 
+public class MovieDriver {
+	public static void main(String[] args)
+	{
+		while (true)
+		{
+		Scanner keyboard = new Scanner(System.in);
+		Movie m = new Movie();
+	
+		System.out.println("Enter the name of a movie");
+		String name = keyboard.nextLine(); 
+		m.setTitle(name);
+		
+		System.out.println("Enter the rating of the movie");
+		String rating = keyboard.nextLine();
+		m.setRating(rating);
+		
+		System.out.println("Enter the number of tickets sold for this movie");
+		int tickets=Integer.parseInt(keyboard.nextLine());
+		m.setSoldTickets(tickets);
+		
+		System.out.println("" +name + " (" +rating + ")" + ": Tickets Sold: " +tickets);
+		System.out.println("Do you want to enter another? (y or n)");
+		String option = keyboard.nextLine();
+
+		if(option.equalsIgnoreCase("n"))
+		{
+			System.out.println("Goodbye");	
+			break;
+		}	
+		}			
+	}	
+}
